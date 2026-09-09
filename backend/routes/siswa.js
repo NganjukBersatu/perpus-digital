b0acbf6b12772ae4358393dcf63036c90efa3706
salmashinta <salmashinta262@gmail.com>
2026-09-09 07:11:44 +0700

    perbaikan backend

--- backend/routes/siswa.js
+++ backend/routes/siswa.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { db } = require('../db/client')
+const db = require('../db')   
 const { anggota, peminjaman, eksemplarBuku, buku } = require('../db/schema')
 const { eq, and, ilike, desc } = require('drizzle-orm')
 
